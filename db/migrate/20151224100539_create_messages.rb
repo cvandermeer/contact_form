fc023f7330134ff30f993f465737f8f61f06230c
Chris van der Meer <cvandermeer@live.nl>
2015-12-24 15:01:17 +0100

    increasing text limit

--- db/migrate/20151224100539_create_messages.rb
+++ db/migrate/20151224100539_create_messages.rb
@@ -3,7 +3,7 @@ class CreateMessages < ActiveRecord::Migration
     create_table :messages do |t|
       t.string :name
       t.string :email
-      t.string :body
+      t.text :body, :text, limit: 4294967295
       t.datetime :created_at
     end
   end
